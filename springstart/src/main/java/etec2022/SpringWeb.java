de4c3c04d69e0099a4cb44e49c6bdbf64f5aa385
PedroMarineli <phmo.1606@gmail.com>
2022-05-16 17:40:19 +0000

    dsf

---
+++ springstart/src/main/java/etec2022/SpringWeb.java
@@ -0,0 +1,11 @@
+package etec2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeb.class, args);
+    }
+}
---
+++ springstart/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Exemplo - SpringBoot</title>
+    </head>
+    <body>
+        <h1>Funciona!!!</h1>
+    </body>
+</html>
